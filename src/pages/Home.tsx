32f0c245abbaf8b1e00e935e9c9850179433f60b
Mateus de Sousa Silva Santos <54694745+mateuso12@users.noreply.github.com>
2021-06-23 18:29:24 -0300

    Criação de página de login e criação de nova sala

---
+++ src/pages/Home.tsx
@@ -0,0 +1,45 @@
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIcon from "../assets/images/google-icon.svg";
+
+import { useHistory } from 'react-router-dom'
+
+import '../styles/auth.scss'
+import { Button } from "../components/Button";
+
+export function Home() {
+  const history = useHistory();
+  
+  function handleCreateRoom() {
+    
+
+    history.push('/rooms/new')
+  }
+
+  return (
+    <div id='page-auth'>
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Crie sala Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
+      <main>
+        <div className='main-content'>
+          <img src={logoImg} alt="LetmeAsk" />
+          <button onClick={navigateToNewRoom} className='create-room'>
+            <img src={googleIcon} alt="Logo google" />
+            <span>Crie sua sala com o Google</span>
+          </button>
+          <div className="separator">ou entre em uma sala</div>
+          <form>
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+}
